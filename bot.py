9092790ac6156e246279af1876f1312f442a0175
adadesions <adadesions@gmail.com>
2019-05-24 16:40:30 +0700

    add comments

--- bot.py
+++ bot.py
@@ -8,15 +8,21 @@ import pyautogui
 #  Click on Reply Button
 pyautogui.click(715, 392)
 
+# Box Boundary for catch an obstacle
 box = (600, 390, 680, 435)
 
 while True:
+    # Grab is like take a screenshot with bbox
     im = ImageGrab.grab(bbox=box)
+
+    # Convert image to Grayscale
     g_im = ImageOps.grayscale(im)
+
+    # Sum all pixels value, normalized by divided by 255.
     sum_ = np.int(np.sum(np.asarray(g_im)/255))
     print("sum: {}".format(sum_))
     if sum_ < 3400:
+        # Press "up" key
         sleep(0.01)
         pyautogui.keyDown("up")
         pyautogui.keyUp("up")
-
